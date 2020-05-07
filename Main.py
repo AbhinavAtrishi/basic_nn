42505103ac8b8f57db88de4b4fe8e22381396eaf
AbhinavAtrishi <abhinav_avm@yahoo.co.in>
2020-05-07 14:30:05 +0530

    Added comments
    Basic design of the code is complete.

--- Main.py
+++ Main.py
@@ -1,15 +1,23 @@
 import numpy as np
-from basic_nn import InputLayer, Sigmoid, TerminalLayer
+from basic_nn import InputLayer, TerminalLayer
 
+# Optional: Set a seed for reproducibility
+np.random.seed(1)
+
+# Current implementation supports 1 layer neural networks. These can learn simple linear separations
+# Here we are implementing a simple AND gate
 x = np.array([[0, 0, 1, 1], [0, 1, 0, 1]])
 y = np.array([0, 0, 0, 1])
 epochs = 1000
 
-il = InputLayer(x)
+# Input Layer
+il = InputLayer(x, (1, 2), 'sigmoid')
+# Terminal Layer used for computing error & back propagation
 tl = TerminalLayer(y)
 
 for itr in range(epochs):
     preds = il.forward()
+    # Set the upstream gradient for the previous layer
     il.upstream_gradient = tl.backprop(preds)
     il.backprop()
 
